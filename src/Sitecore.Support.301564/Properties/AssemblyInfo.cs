42b7b23a0316a62961f43cb32088e9ca8d6d0de3
Pushpraj Ruhal <emai2pushpraj@ruhal.in>
2019-01-01 11:45:49 +0800

    #301564: fixed

---
+++ src/Sitecore.Support.301564/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.301564")]
+[assembly: AssemblyProduct("Sitecore.Support.301564")]
+[assembly: ComVisible(false)]
